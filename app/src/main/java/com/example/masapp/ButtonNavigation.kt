6f4f4101c25bf2e2e0fd68bb4054d99a0f0af3b5
bontran <trongtin.tran99@gmail.com>
2021-11-07 15:16:01 +0700

    bottom nav

---
+++ app/src/main/java/com/example/masapp/ButtonNavigation.kt
@@ -0,0 +1,11 @@
+package com.example.masapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ButtonNavigation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button_navigation)
+    }
+}
--- app/src/main/java/com/example/masapp/MainActivity.kt
+++ app/src/main/java/com/example/masapp/MainActivity.kt
@@ -3,7 +3,13 @@ package com.example.masapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
 import com.example.masapp.databinding.ActivityMainBinding
+import com.example.masapp.ui.fragment.HomeFragment
+import com.example.masapp.ui.fragment.NotifyFragment
+import com.example.masapp.ui.fragment.PersonalFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,6 +20,28 @@ class MainActivity : AppCompatActivity() {
 
         val feelings = resources.getStringArray(R.array.feelings)
         val arrayAdapter = ArrayAdapter(this, R.layout.drop_item, feelings)
-        binding.autoCompleteTextView.setAdapter(arrayAdapter)
+       // binding.autoCompleteTextView.setAdapter(arrayAdapter)
+
+        val homeFragment = HomeFragment();
+        val notificationFragment = NotifyFragment();
+        val personalFragment = PersonalFragment();
+
+        makeCurrentFragment(homeFragment)
+        binding.bottomNavigation.setOnNavigationItemReselectedListener {
+            when(it.itemId) {
+                R.id.home -> makeCurrentFragment(homeFragment)
+                R.id.notification -> makeCurrentFragment(notificationFragment)
+                R.id.personal_profile -> makeCurrentFragment(personalFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
     }
 }
